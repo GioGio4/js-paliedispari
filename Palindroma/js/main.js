eda223596f19970374d3d4fb9d92342902af2e84
GioGio4 <g.ceccobelli@outlook.it>
2023-01-31 13:56:30 +0100

    Init project

---
+++ Palindroma/js/main.js
@@ -0,0 +1,12 @@
+// PALINDROMA - TRACCIA
+// Chiedere all'utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// -------------------------------------------------------------------------------//
+
+// Fai inserire un parola all'utente
+
+// Analizza la parola inserita (con una funzione) e inserisci le lettere al contrario in una variabile
+
+// Controlla se la parola inserita dall'utente è UGUALE a quella (al contrario) inserita nella variabile
+
+// Stampa il risultato 
